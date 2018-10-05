b88a7a7f2fb56d0d617f3c2f8fce8c1beee3b833
Guillaume Caggia <guillaume.caggia@gmail.com>
2018-10-05 22:08:37 +0200

    update for homepage / api depending user agent

--- app.go
+++ app.go
@@ -3,11 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("public"))
-	http.Handle("/", fs)
+
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		user_agent := request.UserAgent()
+		log.Println("user_agent is " + user_agent)
+		if strings.Contains(user_agent, "curl") {
+			writer.Write([]byte("curl!"))
+		} else {
+
+			http.ServeFile(writer, request, "public/" + request.URL.Path[1:])
+		}
+	})
+	// fs := http.FileServer(http.Dir("public"))
+	// http.Handle("/", fs)
+
+
+	http.HandleFunc("/api", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("API Go test!"))
+	})
 
 	log.Println("App is running")
 	log.Println("Curently listening on http://127.0.0.1:5000")
